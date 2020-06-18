ab7661e9e08d2bd6b8dc2985971d3ff1eca4f7d4
aisimba <1546205823@qq.com>
2020-06-18 18:26:12 +0800

    同步复位和异步复位

---
+++ flip_flop/rtl/flip_flop.v
@@ -0,0 +1,16 @@
+module flip_flop(
+    input  sys_clk,
+	 input  sys_rst_n,
+	 input  key_in,
+    
+	 output reg led_out
+);
+
+
+always @(posedge sys_clk or negedge sys_rst_n)begin//同步复位
+	if(sys_rst_n == 1'b0)
+	   led_out <= 1'b0;
+	else
+	  led_out <= key_in;
+end
+endmodule
